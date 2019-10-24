5789beeb65419ff0dc735b881480d76a7aeebca3
Coding Girl <jiayidu@ymail.com>
2019-10-23 20:20:15 -0700

    Update variable.tf

--- terraform/variable.tf
+++ terraform/variable.tf
@@ -1,8 +1,21 @@
   
-variable "aws_region" {
+variable "region" {
   description = "AWS region"
+  default = "us-west-2"
 }
 
 variable "namespace" {
   description = "Unique name to use for DNS and resource naming"
+}
+
+variable "access_key" {
+    description = "AWS access key"
+}
+
+variable "secret_key" {
+    description = "AWS secret key"
+  
+variable "vpc_cidr_block" {
+    description = "AWS VPC CIDR Block to use"
+    default = "10.0.0.0/16"
 }
